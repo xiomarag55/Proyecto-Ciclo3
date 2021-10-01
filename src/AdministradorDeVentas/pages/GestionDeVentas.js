8324d265ab101fd3843a77a23673748279bdea07
xiomarag55 <xiomaragiraldo221@gmail.com>
2021-10-01 18:36:36 -0500

    Se elimina el nav.

--- src/AdministradorDeVentas/pages/GestionDeVentas.js
+++ src/AdministradorDeVentas/pages/GestionDeVentas.js
@@ -84,23 +84,6 @@ const GestionDeVentas = () => {
 
     return (
         <>
-            <Navbar className="Navbar">
-                <Container fluid>
-                    <Navbar.Brand className="text-white" href="#">ChocoApp</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav className="me-auto my-2 my-lg-0">
-                            <Nav.Link className="text-white" href="#action1">Home</Nav.Link>
-                            <Nav.Link className="text-white" href="#action2">Productos</Nav.Link>
-                            <Nav.Link className="text-white" href="#" >
-                                Usuarios
-                            </Nav.Link>
-                        </Nav>
-                        <Button variant="outline-info">Logout</Button>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-
             <Container>
                 <Row className="d-flex justify-content-center align-items-center">
                     <Col xs={20}>
