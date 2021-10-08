8c0d5e281b3bf34470424f7a973c04000edc1d69
Karen <karenkaob6@gmail.com>
2021-10-08 13:10:39 -0500

    endpoint delete 3.0

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,5 +5,6 @@ const UserController = require("../controllers/users");
 
 router.get("", UserController.getUsers);
 router.post("", UserController.addUser);
+router.delete("",UserController.deleteUser)
 
 module.exports = router;
