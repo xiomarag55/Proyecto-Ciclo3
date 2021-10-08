bb3eb79cf9570d04178787642e199526d84ce31c
Karen <karenkaob6@gmail.com>
2021-10-07 20:49:30 -0500

    se crean carpetas de usuarios en backend

---
+++ backend/Models/users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const usuario = mongoose.Schema({
+    identificacion: { type: String, required: true },
+    name: { type:String, required: true },
+    role: { type: String, required: true },
+    status: { type: String, required: true }
+  });
+  module.exports = mongoose.model("Usuario", usuario);
