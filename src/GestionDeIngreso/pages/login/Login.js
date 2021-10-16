ccc3aa2704a480ddcf1bfbc7555ba7c2d948bbed
xiomarag55 <xiomaragiraldo221@gmail.com>
2021-10-15 22:15:45 -0500

    Se cambia el botón de login.

--- src/GestionDeIngreso/pages/login/Login.js
+++ src/GestionDeIngreso/pages/login/Login.js
@@ -1,43 +1,42 @@
-import React from 'react';
+import React from "react";
 import "./Login.css";
-import ChocoApp2 from './ChocoApp2.png';
-import fondo from './fondo.png';
-import {GoogleLogin} from 'react-google-login';
+import ChocoApp2 from "./ChocoApp2.png";
+import fondo from "./fondo.png";
+import GoogleLogin from "react-google-login";
 
+const loginForm = () => {
+  const login = (res) => {
+    console.log(res);
+  };
+  const loginError = (res) => {
+    console.log(res);
+  };
+  return (
+    <>
+      <form>
+        <div class="contenedor">
+          <div class="centered">
+            <img src={ChocoApp2} />
+          </div>
+          <div class="ingreso">
+            <GoogleLogin
+              clientId="23199630294-vhvoq6u3t3fh31oa1on95ionv308n62v.apps.googleusercontent.com"
+              buttonText="Iniciar sesión"
+              onSuccess={login}
+              onFailure={loginError}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
 
-const loginForm=()=>{
-    return (
-        <form>
-            <div class="contenedor">
-                <div class="centered">
-                    <img src={ChocoApp2}/>
-                </div>
-                <div class="ingreso">
-                    <GoogleLogin
-                        clientId='23199630294-vhvoq6u3t3fh31oa1on95ionv308n62v.apps.googleusercontent.com'
-                        buttonText="Iniciar sesión"
-                        onSuccess={login}
-                        onFailure={loginError}
-                        cookiePolicy={"single-host-origin"}
-                    />
-                </div>
-                
-                <div className="contenedor2">
-                    
-                    <div class="back">
-                        <img src={fondo}/>
-                    </div>
-                </div>
-                
-            </div>    
-        </form>
-    )
-}
-const loginError=()=>{
+          <div className="contenedor2">
+            <div class="back">
+              <img src={fondo} />
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
 
-}
-const login=()=>{
-    
-
-}
 export default loginForm;
